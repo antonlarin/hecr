b825e4fa0c5c1b3705b211d97d3b017338beb8bd
Nikita-K <n.yu.kudriavtsev@gmail.com>
2016-05-28 18:40:39 +0300

    Seq version was changed

--- seq/solver.cpp
+++ seq/solver.cpp
@@ -1,49 +1,55 @@
-#include <fstream>
-#include <chrono>
-
 #include "solver.hpp"
 
 vec cyclic_reduction(const TridiagonalMatrix& m, const vec& b)
 {
-	std::vector<vec > a_vecs;
-	a_vecs.push_back(m.as);
-	std::vector<vec > b_vecs;
-	b_vecs.push_back(m.bs);
-	std::vector<vec > c_vecs;
-	c_vecs.push_back(m.cs);
-	std::vector<vec > rhs_vecs;
-	rhs_vecs.push_back(b);
 	int equation_count = m.size;
+	int reductions_count = static_cast<int>(floor(log(equation_count) /
+		log(2)));
+	int coeff_array_size = 2 * (equation_count + 1) - reductions_count - 3;
+	double* a_vecs = new double[coeff_array_size];
+	double* b_vecs = new double[coeff_array_size];
+	double* c_vecs = new double[coeff_array_size];
+	double* rhs_vecs = new double[coeff_array_size];
+	memcpy(a_vecs, m.as.data(), equation_count * sizeof(double));
+	memcpy(b_vecs, m.bs.data(), equation_count * sizeof(double));
+	memcpy(c_vecs, m.cs.data(), equation_count * sizeof(double));
+	memcpy(rhs_vecs, b.data(), equation_count * sizeof(double));
+	int base_idx = equation_count;
 
 	while (equation_count > 1)
 	{
+		int old_base_idx = base_idx - equation_count;
 		equation_count /= 2;
-		vec new_a(equation_count, 0.0);
-		vec new_b(equation_count, 0.0);
-		vec new_c(equation_count, 0.0);
-		vec new_rhs(equation_count, 0.0);
-		vec& old_a = a_vecs.back();
-		vec& old_b = b_vecs.back();
-		vec& old_c = c_vecs.back();
-		vec& old_rhs = rhs_vecs.back();
+
+		double* old_a = a_vecs + old_base_idx;
+		double* old_b = b_vecs + old_base_idx;
+		double* old_c = c_vecs + old_base_idx;
+		double* old_rhs = rhs_vecs + old_base_idx;
+		double* new_a = a_vecs + base_idx;
+		double* new_b = b_vecs + base_idx;
+		double* new_c = c_vecs + base_idx;
+		double* new_rhs = rhs_vecs + base_idx;
 
 		for (int i = 0; i < equation_count; i++)
 		{
 			int old_i = 2 * i + 1;
+			double oldA_next = old_a[old_i + 1];
+			double oldA_prev = old_a[old_i - 1];
+
+			double CA = old_c[old_i] / oldA_next;
+			double BA = old_b[old_i] / oldA_prev;
+
 			new_a[i] = old_a[old_i] -
-				old_b[old_i] * old_c[old_i - 1] / old_a[old_i - 1] -
-				old_c[old_i] * old_b[old_i + 1] / old_a[old_i + 1];
-			new_b[i] = -old_b[old_i - 1] * old_b[old_i] / old_a[old_i - 1];
-			new_c[i] = -old_c[old_i] * old_c[old_i + 1] / old_a[old_i + 1];
+				BA * old_c[old_i - 1] -
+				CA * old_b[old_i + 1];
+			new_b[i] = -old_b[old_i - 1] * BA;
+			new_c[i] = -CA * old_c[old_i + 1];
 			new_rhs[i] = old_rhs[old_i] -
-				old_rhs[old_i - 1] * old_b[old_i] / old_a[old_i - 1] -
-				old_rhs[old_i + 1] * old_c[old_i] / old_a[old_i + 1];
+				old_rhs[old_i - 1] * BA -
+				old_rhs[old_i + 1] * CA;
 		}
 
-		a_vecs.push_back(new_a);
-		b_vecs.push_back(new_b);
-		c_vecs.push_back(new_c);
-		rhs_vecs.push_back(new_rhs);
+		base_idx += equation_count;
 	}
 
 	// result is padded with one zero one both sides to emulate
@@ -52,31 +58,34 @@ vec cyclic_reduction(const TridiagonalMatrix& m, const vec& b)
 	// factor maps indices of equations in full system extended with x_0=0
 	// and x_N=0 onto indices of equations in reduced extended system
 	int factor = m.size / 2 + 1;
-	for (unsigned int i = a_vecs.size(); i-- > 0;)
+	int n = 1;
+	for (unsigned int i = reductions_count + 1; i-- > 0;)
 	{
-		vec& as = a_vecs[i];
-		vec& bs = b_vecs[i];
-		vec& cs = c_vecs[i];
-		vec& rhs = rhs_vecs[i];
+		base_idx -= equation_count;
+		double* as = a_vecs + base_idx;
+		double* bs = b_vecs + base_idx;
+		double* cs = c_vecs + base_idx;
+		double* rhs = rhs_vecs + base_idx;
 
 		// j is an index of an equation in the reduced system extended with
 		// x_0=0 and x_N=0
-		
-		for (int j = 1; j < equation_count + 1; j += 2)
+		for (int j = 1; j < n + 1; j++)
 		{
+			int k = j * 2 - 1;
 			// index in coefficents arrays is less by 1 since they don't
 			// include x_0=0 and x_N=0 equations
-			int coeff_j = j - 1;
+			int coeff_j = k - 1;
 			// mapping j to the index in the full extended system
-			int result_j = j * factor;
+			int result_j = k * factor;
 			int result_j_minus1 = result_j - factor;
 			int result_j_plus1 = result_j + factor;
-			
+
 			result[result_j] =
 				(rhs[coeff_j] - bs[coeff_j] * result[result_j_minus1] -
 				cs[coeff_j] * result[result_j_plus1]) / as[coeff_j];
 		}
 
+		n = equation_count + 1;
 		equation_count = equation_count * 2 + 1;
 		factor /= 2;
 	}
@@ -84,6 +93,11 @@ vec cyclic_reduction(const TridiagonalMatrix& m, const vec& b)
 	// drop the padding
 	result.pop_back();
 	result.erase(result.begin());
+
+	delete[] a_vecs;
+	delete[] b_vecs;
+	delete[] c_vecs;
+	delete[] rhs_vecs;
 
 	return result;
 }
@@ -102,21 +116,13 @@ double solve_problem(int nx, int nt)
 
 	TridiagonalMatrix m = construct_matrix(nx, nt);
 
-	std::chrono::duration<double> elapsed_time, elapsed_timeOnce;
-	std::fstream timing_file("timeReduct.txt", std::ios::out);
-
 	for (int i = 1; i <= nt; i++)
 	{
 		vec b = construct_rhs(nx, nt, current_layer, i * tau,
 			f, exact_solution);
 
-		auto start_time = std::chrono::high_resolution_clock::now();
 		current_layer = cyclic_reduction(m, b);
-		auto end_time = std::chrono::high_resolution_clock::now();
-		elapsed_time += end_time - start_time;
 	}
-
-	timing_file << elapsed_time.count() << std::endl;
 
 	double error = compute_error(current_layer, nx);
 
@@ -143,4 +149,3 @@ vec construct_rhs(int nx, int nt, const vec& previous_layer, double t,
 
 	return rhs;
 }
-
